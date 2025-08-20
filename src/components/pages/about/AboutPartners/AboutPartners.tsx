a5a601ebb679222c7a924384603b61e98a56d22a
Emin Mammadov <eminmammadovcom@gmail.com>
2025-08-20 21:32:31 +0400

    upsates

--- src/components/pages/about/AboutPartners/AboutPartners.tsx
+++ src/components/pages/about/AboutPartners/AboutPartners.tsx
@@ -21,121 +21,121 @@ const AboutPartners: React.FC<AboutPartnersProps> = ({ className = '' }) => {
     {
       id: '1',
       name: 'Switchboard',
-      logo: '/images/partners/switchboard.svg',
+      logo: 'https://docs.switchboard.xyz/img/logo.svg',
       alt: 'Switchboard logo'
     },
     {
       id: '2',
       name: 'WithoutFee',
-      logo: '/images/partners/withoutfee.svg',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/ethereum.svg',
       alt: 'WithoutFee logo'
     },
     {
       id: '3',
       name: 'XRP Ledger',
-      logo: '/images/partners/xrp-ledger.svg',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/xrp.svg',
       alt: 'XRP Ledger logo'
     },
     {
       id: '4',
       name: 'BlackRock',
-      logo: '/images/partners/blackrock.svg',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/blackrock.svg',
       alt: 'BlackRock logo'
     },
     {
       id: '5',
       name: 'Ripple',
-      logo: '/images/partners/ripple.svg',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/ripple.svg',
       alt: 'Ripple logo'
     },
     {
       id: '6',
       name: 'Franklin Templeton',
-      logo: '/images/partners/franklin-templeton.svg',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/franklin.svg',
       alt: 'Franklin Templeton logo'
     },
     {
       id: '7',
       name: 'Gitcoin',
-      logo: '/images/partners/gitcoin.svg',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/gitcoin.svg',
       alt: 'Gitcoin logo'
     },
     {
       id: '8',
       name: 'Jupiter',
-      logo: '/images/partners/jupiter.svg',
+      logo: 'https://station.jup.ag/img/jupiter-logo.svg',
       alt: 'Jupiter logo'
     },
     {
       id: '9',
       name: 'Coinbase',
-      logo: '/images/partners/coinbase.svg',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/coinbase.svg',
       alt: 'Coinbase logo'
     },
     {
       id: '10',
       name: 'Phantom',
-      logo: '/images/partners/phantom.svg',
+      logo: 'https://phantom.app/img/phantom-logo.svg',
       alt: 'Phantom logo'
     },
     {
       id: '11',
       name: 'Solana',
-      logo: '/images/partners/solana.svg',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/solana.svg',
       alt: 'Solana logo'
     },
     {
       id: '12',
       name: 'Polygon',
-      logo: '/images/partners/polygon.svg',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/polygon.svg',
       alt: 'Polygon logo'
     },
     {
       id: '13',
       name: 'Arbitrum',
-      logo: '/images/partners/arbitrum.svg',
+      logo: 'https://arbitrum.io/wp-content/uploads/2021/01/cropped-Arbitrum_Symbol-Full-color-White-background-1.svg',
       alt: 'Arbitrum logo'
     },
     {
       id: '14',
       name: 'Chainlink',
-      logo: '/images/partners/chainlink.svg',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/chainlink.svg',
       alt: 'Chainlink logo'
     },
     {
       id: '15',
       name: 'Fidelity',
-      logo: '/images/partners/fidelity.svg',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/fidelity.svg',
       alt: 'Fidelity logo'
     },
     {
       id: '16',
       name: 'PayPal',
-      logo: '/images/partners/paypal.svg',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/paypal.svg',
       alt: 'PayPal logo'
     },
     {
       id: '17',
       name: 'LayerZero',
-      logo: '/images/partners/layerzero.svg',
+      logo: 'https://layerzero.network/img/LayerZero_Logo_White.svg',
       alt: 'LayerZero logo'
     },
     {
       id: '18',
-      name: 'Meta Mask',
-      logo: '/images/partners/metamask.svg',
-      alt: 'Meta Mask logo'
+      name: 'MetaMask',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/metamask.svg',
+      alt: 'MetaMask logo'
     },
     {
       id: '19',
       name: 'Axelar',
-      logo: '/images/partners/axelar.svg',
+      logo: 'https://axelar.network/wp-content/uploads/2022/01/axelar-logo.svg',
       alt: 'Axelar logo'
     },
     {
       id: '20',
       name: 'Stellar',
-      logo: '/images/partners/stellar.svg',
+      logo: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/stellar.svg',
       alt: 'Stellar logo'
     }
   ];
