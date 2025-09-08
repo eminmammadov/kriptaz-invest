57683bcc4814f2d439fafc712c6d43a9b3f94c0b
Emin Mammadov <eminmammadovcom@gmail.com>
2025-09-08 12:53:22 +0400

    updates

--- src/app/(utils)/sitemap.ts
+++ src/app/(utils)/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://kriptaz-invest.com'
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://investfounders.com'
   
   return [
     {
